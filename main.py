2aa8ddb77b8716cf09ef707c12b24dbc62564773
Joe Roth <joe.roth.95@web.de>
2025-04-21 17:47:09 +0200

    A: created window and exit loops

---
+++ main.py
@@ -0,0 +1,24 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+import constants
+from constants import *
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
